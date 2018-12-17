10a47296374b3d978fa60d921f8c5575e4865f9a
Milind <44804793+MilindDev17@users.noreply.github.com>
2018-12-17 21:44:34 +0530

    xor

---
+++ XorSum/xor.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class X {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int nx = sc.nextInt();
+		int ny = sc.nextInt();
+		//sum of xor
+			int sum = 0;
+			int[] arr = new int[n+1];
+		for(int i = 0;i<n;i++){
+			int z = arr[i]
+		}
+		
+		for(int i = 0;i < arr.length();i++) {
+			for(int j = 0;j<arr.length;j++)
+			   sum = sum ^ (arr[i]);	
+		return sum;
+		}
+ 	}}
+	
+	
+	/*
+		sum = { 1 2 3 }
+				1 , 1 + 2 , 1 + 2 + 3 
+	*/
